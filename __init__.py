d95edb9c200464907c6ec0e16adefd7e2a7c32c1
Martin Engel <martin.engel@agineo.de>
2025-06-03 20:00:41 +0200

    Moved files into new subfolder MyFramework

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+Durch das Framework werden Module bereitgestellt, die in diversen Projekten hilfreich sein können, indem sie allgemeingültige Klassen und wiederkehrende Funktionen bereitstellen.
+
+<h5>Framework.Informations.Informations</h5>
+Die Idee dieser Klasse folgt zwei Szenarien. Hinweise, die während der Ausführung einer Anwendung erfasst werden sollen, werden
+<li> ... in einem Objekt zentral gespeichert und für anschließende Aufrufe bereitgestellt. Diese Informationen können entsprechend weiterführender Anwendungsfälle angezeigt, gespeichert oder analysiert werden.
+<li> ... über einheitliche und leicht zugängliche Funktionen direkt im Terminal ausgegeben.
+
+Die Informationen werden typisiert (siehe `Framework.Informations.Informations.Types`). Es werden automatisch Metainformationen wie Dateiname, Funktion und Zeilennummer erfasst.
+
+<h5>Framework.Data</h5>
+Funktionen in diesem Modul dienen dem Anzeigen, Analysieren und Manipulieren von Datenstrukturen.
+"""
